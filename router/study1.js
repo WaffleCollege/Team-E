cd9632c228ed4b8450b3f88f613910231e6efa44
Rina Sakamoto <rnshtk3009@gmail.com>
2023-03-11 23:43:18 +0900

    body-parser読み込み & inputで入力した名前がcommentsの下に出力されるようにした

--- router/study1.js
+++ router/study1.js
@@ -1,8 +1,40 @@
 const express = require("express");
 const router = express.Router();
 
+const bodyParser = require("body-parser");
+
+const names = [];
+// const links = [];
+// const comments = [];
+
+//middleware
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
 router.get('/studypage', (req, res) => {
-	res.render('studypage.ejs');
+  // console.log(names);
+	res.render('studypage.ejs', {
+    names: names, 
+  });
 });
 
+router.post("/studypage", (req, res, next) => {
+  const n = req.body.username;
+  names.push(n);
+  res.redirect('studypage');
+});
+
+// router.post("/studypage", (req, res, next) => {
+//   links.push({ url: req.body.link });
+//   res.redirect("/studypage");
+// });
+
+// router.post("/studypage", (req, res, next) => {
+//   comments.push({ comment: req.body.comment });
+//   res.redirect("/studypage");
+// });
+
 module.exports = router;
+// module.exports = names;
+// module.exports = links;
+// module.exports = comments;
